d8b2d6dc9875aedabec67df2179d1b54471def02
χ <voidbyte@null.net>
2025-11-04 18:08:37 +0100

    + list commits in a given branch

--- crates/lib-git/src/repo.rs
+++ crates/lib-git/src/repo.rs
@@ -1,5 +1,8 @@
-use crate::{Commit, error::Result};
-use git2::{Oid, Repository};
+use crate::{
+	Commit,
+	error::{Error, Result},
+};
+use git2::{Oid, Repository, Revwalk, Sort};
 use std::path::Path;
 
 pub struct Repo {
@@ -11,6 +14,24 @@ impl Repo {
 		Ok(Self {
 			inner: Repository::discover(path)?,
 		})
+	}
+
+	pub fn list_commits_in_branch(&self, branch: &str) -> Result<Vec<Commit>> {
+		let branch = self.inner.find_branch(branch, git2::BranchType::Local)?;
+		let branch = branch.into_reference();
+
+		let id = branch.target().ok_or_else(|| Error::InvalidBranchTarget)?;
+		let mut revwalk = self.inner.revwalk()?;
+		revwalk.push(id)?;
+		revwalk.set_sorting(Sort::TIME)?;
+
+		let mut commits = Vec::new();
+		for commit_id in revwalk {
+			let commit_id = commit_id?;
+			let commit = self.find_commit(commit_id)?;
+			commits.push(commit);
+		}
+		Ok(commits)
 	}
 
 	pub fn find_commit(&self, commit_id: Oid) -> Result<Commit> {
